d428f8b6704ec34aa68deb84897a927355ba9a70
Daniel Supernault <danielsupernault@gmail.com>
2025-09-08 04:25:10 -0600

    Update vue-query.ts

--- plugins/vue-query.ts
+++ plugins/vue-query.ts
@@ -41,7 +41,7 @@ export default defineNuxtPlugin((nuxt) => {
                 queryClient,
                 persister: localStoragePersister,
                 maxAge: 1000 * 60 * 60 * 24 * 14, // 14 days
-                buster: 'v1.4',
+                buster: 'v1.5',
                 dehydrateOptions: {
                     shouldDehydrateQuery: (query) => {
                         return (
